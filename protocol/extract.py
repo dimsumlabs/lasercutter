6204cd910af71d8f44af638bb2212c8c5997853a
Hamish Coleman <hamish@zot.org>
2017-01-27 16:36:36 +0800

    Add some more decoding to the extractor

--- protocol/extract.py
+++ protocol/extract.py
@@ -5,9 +5,43 @@
 import sys
 import pcap
 
+timestamp_first = None
+
 def packet_handler(datalen, data, timestamp):
-    if len(data)>0x41 and data[0x40]=='\xa6':
-        print data[0x42:]
+    global timestamp_first
+    if timestamp_first is None:
+        timestamp_first = timestamp
+
+    timestamp_delta = timestamp - timestamp_first
+
+    transfer_types = {
+        '\x02': 'URB_CONTROL',
+        '\x03': 'URB_BULK',
+    }
+    urb_type = data[8]
+    transfer_type = transfer_types[data[9]]
+    if ord(data[10]) & 0x80:
+        direction = "<"
+    else:
+        direction = ">"
+
+    # Hide packets that are basically noise
+    # - USB sends an empty setup packet for every reply packet
+    # - and an empty complete packet ACK for every transmit
+    
+    show_packet = True
+    if urb_type=='S' and transfer_type=='URB_BULK' and direction=='<':
+        show_packet = False
+    if urb_type=='C' and transfer_type=='URB_BULK' and direction=='>':
+        show_packet = False
+
+    if show_packet:
+        print "{:7.2f} {} {} {}".format(
+            timestamp_delta,
+            transfer_type,
+            direction,
+            data[0x40:]
+        )
 
 def main():
     p = pcap.pcapObject()
